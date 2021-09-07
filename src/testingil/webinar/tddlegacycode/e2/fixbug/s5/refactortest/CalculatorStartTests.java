f9babf4d26eca2c44453fd06781c06c0c21f9805
gilzilberfeld <gil.zilberfeld@gmail.com>
2021-09-07 13:26:40 +0300

    refactor tests

--- src/testingil/webinar/tddlegacycode/e2/fixbug/s5/refactortest/CalculatorStartTests.java
+++ src/testingil/webinar/tddlegacycode/e2/fixbug/s5/refactortest/CalculatorStartTests.java
@@ -5,20 +5,20 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class BugFixesTests extends BaseCalculatorTests{
+class CalculatorStartTests extends BaseCalculatorTests{
 
 	@Test
-	void when_pressing_plus_at_start_should_show_0() {
+	void when_pressing_plus_should_show_0() {
 		pressing("+").shouldShow("0");
 	}
 
 	@Test
-	void when_pressing_div_at_start_should_show_0() {
+	void when_pressing_div_should_show_0() {
 		pressing("/").shouldShow("0");
 	}
 
 	@Test
-	void when_pressing_equal_at_start_should_show_0() {
+	void when_pressing_equal_should_show_0() {
 		pressing("=").shouldShow("0");
 	}
 }
