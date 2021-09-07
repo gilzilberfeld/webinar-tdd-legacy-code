5c7153135018edcc0289fc33ab7f23f5e6fdcabd
gilzilberfeld <gil.zilberfeld@gmail.com>
2021-09-07 12:57:58 +0300

    s3 better fix

---
+++ src/testingil/webinar/tddlegacycode/e2/fixbug/s3/betterfix/Calculator.java
@@ -0,0 +1,64 @@
+package testingil.webinar.tddlegacycode.e2.fixbug.s3.betterfix;
+
+public class Calculator {
+	String display = "";
+	int lastArgument = 0;
+	int result = 0; 
+	Boolean newArgument = false;
+	Boolean shouldReset = true;
+	
+	OperationType lastOperation;
+
+	
+	public void press(String key) {
+		if (key.equals("C"))
+			display = "";
+		else {
+			if (key.equals("+")) {
+				if (display != "") {
+					lastOperation = OperationType.Plus;	
+					lastArgument =  Integer.parseInt(display);
+					newArgument = true;
+				}
+				} else {
+				if (key.equals("/")) {
+					if (display != "") {
+					lastOperation = OperationType.Div;	
+					lastArgument =  Integer.parseInt(display);
+					newArgument = true;
+					}
+				}
+				else if (key.equals("=")) {
+					if (display != "") {
+					int currentArgument = Integer.parseInt(display);
+					if (lastOperation == OperationType.Plus) {
+						display = Integer.toString(lastArgument + currentArgument);
+					}
+					if (lastOperation == OperationType.Div && currentArgument == 0) {
+						display = "Division By Zero Error";
+					}
+					shouldReset = true;
+					}
+				}
+				else {
+					if (shouldReset) {
+						display = "";
+						shouldReset = false;
+					}
+					if (newArgument) {
+						display ="";
+						newArgument = false;
+					}
+					display += key;
+				}
+			}
+		}
+	}
+
+	public String getDisplay() {
+		if (display.equals(""))
+			return "0";
+		return display;
+	}
+
+}
---
+++ src/testingil/webinar/tddlegacycode/e2/fixbug/s3/betterfix/OperationType.java
@@ -0,0 +1,6 @@
+package testingil.webinar.tddlegacycode.e2.fixbug.s3.betterfix;
+
+public enum OperationType {
+	Plus, 
+	Div
+}
