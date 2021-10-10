3c39d3ac21e4bbf58d62ff2b4cd51d89bff4ba49
gilzilberfeld <gil.zilberfeld@gmail.com>
2021-10-10 11:54:41 +0300

    fixed it

--- src/testingil/webinar/tddlegacycode/e0/start/theTest.java
+++ src/testingil/webinar/tddlegacycode/e0/start/theTest.java
@@ -6,47 +6,8 @@ import org.junit.jupiter.api.Test;
 
 class theTest {
 
-	private Calculator calc;
-
-	@BeforeEach
-	public void setup(){
-		calc = new Calculator();
-	}
-
 	@Test
-	void successes() {
-		assertEquals("0", calc.getDisplay());
-		press_and_check("3", "3");
-		press_and_check("3+", "3");
-		press_and_check("3+4", "4");
-		press_and_check("3+4+", "4");
-		press_and_check("3+4=", "7");
-		press_and_check("3C", "0");
-		press_and_check("6/3", "3");
-		press_and_check("6/0", "0");
-		press_and_check("6/0=", "Division By Zero Error");
-	}
-
-	@Test
-	void failures() {
-		press_and_check("+", "0");
-		press_and_check("03", "3");
-		press_and_check("6/3=", "2");
-		press_and_check("X", "0");
-		press_and_check("%", "0");
-	}
-	
-	@Test
-	void works_but_should_it() {
-		press_and_check("3++", "3");
-		
-	}
-	private void press_and_check(String param, String expected) {
-		param.chars().forEach(c ->{
-			calc.press(String.valueOf((char) c));
-		}); 
-		assertEquals(expected, calc.getDisplay());
-		calc.press("C");
+	public void firstTest(){
 	}
 
 }
--- src/testingil/webinar/tddlegacycode/e3/addfunctionality/s1/refactor/s3/opextract/CharacaterizationTests.java
+++ src/testingil/webinar/tddlegacycode/e3/addfunctionality/s1/refactor/s3/opextract/CharacaterizationTests.java
@@ -8,16 +8,16 @@ class CharacaterizationTests extends BaseCalculatorTests{
 
 	@Test
 	void successes() {
-//		atStart().shouldShow("0");
-//		pressing("C").shouldShow("0");
-//		pressing("3").shouldShow("3");
-//		pressing("3+").shouldShow("3");
-//		pressing("3+4").shouldShow("4");
-//		pressing("3+4+").shouldShow("4");
-//		pressing("3+4=").shouldShow("7");
-//		pressing("3C").shouldShow("0");
-//		pressing("6/3").shouldShow("3");
-//		pressing("6/0").shouldShow("0");
+		atStart().shouldShow("0");
+		pressing("C").shouldShow("0");
+		pressing("3").shouldShow("3");
+		pressing("3+").shouldShow("3");
+		pressing("3+4").shouldShow("4");
+		pressing("3+4+").shouldShow("4");
+		pressing("3+4=").shouldShow("7");
+		pressing("3C").shouldShow("0");
+		pressing("6/3").shouldShow("3");
+		pressing("6/0").shouldShow("0");
 		pressing("6/0=").shouldShow("Division By Zero Error");
 	}
 
