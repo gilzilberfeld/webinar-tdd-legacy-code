6bf9e3e793d88017409b3f2b7b8cb3885e17e3b9
gilzilberfeld <gil.zilberfeld@gmail.com>
2021-11-14 12:33:51 +0200

    E2 s2

---
+++ C#/E2.FixBug/S2.Fix/BugFixTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDDLegacyCode.E2.FixBug.S2.Fix
+{
+    [TestClass]
+
+    public class BugFixTests
+    {
+        private Calculator calc;
+        private string result;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            calc = new Calculator();
+        }
+
+        [TestMethod]
+        public void When_pressing_operation_at_start_should_show_0()
+        {
+            Pressing("+").ShouldShow("0");
+        }
+
+        private BugFixTests Pressing(string keys)
+        {
+            calc.Press("C");
+            foreach (char c in keys)
+            {
+                calc.Press(c.ToString());
+            };
+            this.result = calc.GetDisplay();
+            return this;
+        }
+
+
+        private void ShouldShow(string expected)
+        {
+            Assert.AreEqual(expected, result);
+
+        }
+
+    }
+}
---
+++ C#/E2.FixBug/S2.Fix/OperationType.cs
@@ -0,0 +1,8 @@
+﻿namespace TDDLegacyCode.E2.FixBug.S2.Fix
+{
+    public enum OperationType
+    {
+        Plus,
+        Div
+    }
+}
