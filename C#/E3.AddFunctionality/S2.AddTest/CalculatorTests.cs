7828efc09f46411d490127296a4ded17f50b39fb
gilzilberfeld <gil.zilberfeld@gmail.com>
2021-11-14 15:47:24 +0200

    E3 S2

---
+++ C#/E3.AddFunctionality/S2.AddTest/CalculatorTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDLegacyCode.E3.AddFunctionality.S2.AddTest
+{
+    [TestClass]
+    public class CalculatorTests : BaseCalculatorTests
+    {
+        [TestMethod]
+        public void Number_after_minus_should_still_display()
+        {
+            Pressing("9-").ShouldShow("9");
+        }
+    }
+}
---
+++ C#/E3.AddFunctionality/S2.AddTest/OperationType.cs
@@ -0,0 +1,8 @@
+﻿namespace TDDLegacyCode.E3.AddFunctionality.S2.AddTest
+{
+    public enum OperationType
+    {
+        Plus,
+        Div
+    }
+}
