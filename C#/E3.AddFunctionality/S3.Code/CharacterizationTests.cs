c1fecc6997577636b1b16f8101a8b8a4ab0cf119
gilzilberfeld <gil.zilberfeld@gmail.com>
2021-11-14 15:49:49 +0200

    E3 S3

---
+++ C#/E3.AddFunctionality/S3.Code/CharacterizationTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDDLegacyCode.E3.AddFunctionality.S3.Code
+{
+    [TestClass]
+    public class CharacterizationTests : BaseCalculatorTests
+    {
+
+        [TestMethod]
+        public void Successes()
+        {
+            AtStart().ShouldShow("0");
+            Pressing("C").ShouldShow("0");
+            Pressing("3").ShouldShow("3");
+            Pressing("3+").ShouldShow("3");
+            Pressing("3+4").ShouldShow("4");
+            Pressing("3+4+").ShouldShow("4");
+            Pressing("3+4=").ShouldShow("7");
+            Pressing("3C").ShouldShow("0");
+            Pressing("6/3").ShouldShow("3");
+            Pressing("6/0").ShouldShow("0");
+            Pressing("6/0=").ShouldShow("Division By Zero Error");
+        }
+
+    }
+}
---
+++ C#/E3.AddFunctionality/S3.Code/OperationType.cs
@@ -0,0 +1,9 @@
+﻿namespace TDDLegacyCode.E3.AddFunctionality.S3.Code
+{
+    public enum OperationType
+    {
+        Plus,
+        Div,
+        Minus
+    }
+}
