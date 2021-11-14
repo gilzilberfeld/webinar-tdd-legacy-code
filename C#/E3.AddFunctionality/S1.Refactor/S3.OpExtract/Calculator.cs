0f73ed35acc1371110151cbd2c77749e02db9a09
gilzilberfeld <gil.zilberfeld@gmail.com>
2021-11-14 13:15:03 +0200

    E3 S1 S3

---
+++ C#/E3.AddFunctionality/S1.Refactor/S3.OpExtract/Calculator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace TDDLegacyCode.E3.AddFunctionality.S1.Refactor.S3.OpExtract
+{
+    public class Calculator
+    {
+        string display = "";
+        int lastArgument = 0;
+        int result = 0;
+        bool newArgument = false;
+        bool shouldReset = true;
+
+        OperationType lastOperation;
+
+
+        public void Press(string key)
+        {
+            if (key.Equals("C"))
+            {
+                display = "";
+                return;
+            }
+            switch (key)
+            {
+                case "+":
+                    HandleOpKey(OperationType.Plus);
+                    break;
+                case "/":
+                    HandleOpKey(OperationType.Div);
+                    break;
+                case "=":
+                    int currentArgument = ParseKeyNumber();
+                    if (lastOperation == OperationType.Plus)
+                    {
+                        display = (lastArgument + currentArgument).ToString();
+                    }
+
+                    if (lastOperation == OperationType.Div && currentArgument == 0)
+                    {
+                        display = "Division By Zero Error";
+                    }
+                    shouldReset = true;
+                    break;
+                default:
+                    if (shouldReset)
+                    {
+                        display = "";
+                        shouldReset = false;
+                    }
+                    if (newArgument)
+                    {
+                        display = "";
+                        newArgument = false;
+                    }
+                    display += key;
+                    break;
+            }
+        }
+
+        private void HandleOpKey(OperationType op)
+        {
+            lastOperation = op;
+            lastArgument = ParseKeyNumber();
+            newArgument = true;
+        }
+
+        private int ParseKeyNumber()
+        {
+            if (display.Equals(""))
+                return 0;
+            return Int32.Parse(display);
+        }
+
+        public String GetDisplay()
+        {
+            if (display.Equals(""))
+                return "0";
+            return display;
+        }
+    }
+
+}
+
---
+++ C#/E3.AddFunctionality/S1.Refactor/S3.OpExtract/OperationType.cs
@@ -0,0 +1,8 @@
+﻿namespace TDDLegacyCode.E3.AddFunctionality.S1.Refactor.S3.OpExtract
+{
+    public enum OperationType
+    {
+        Plus,
+        Div
+    }
+}
