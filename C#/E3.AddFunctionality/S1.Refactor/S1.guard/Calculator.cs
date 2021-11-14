838c9b3a91d9266ebaf6fea27e32b25ad5650219
gilzilberfeld <gil.zilberfeld@gmail.com>
2021-11-14 13:04:07 +0200

    E3 S1 S1

---
+++ C#/E3.AddFunctionality/S1.Refactor/S1.guard/Calculator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace TDDLegacyCode.E3.AddFunctionality.S1.Refactor.S1.Guard
+{
+    public class Calculator
+    {
+        string display = "";
+        int lastArgument = 0;
+        int result = 0;
+        bool newArgument = false;
+        bool shouldReset = true;
+
+        OperationType lastOperation;
+
+
+        public void Press(string key)
+        {
+            if (key.Equals("C"))
+            {
+                display = "";
+                return;
+            }
+            if (key.Equals("+"))
+            {
+
+                lastOperation = OperationType.Plus;
+                lastArgument = ParseKeyNumber();
+                newArgument = true;
+            }
+            else
+            {
+                if (key.Equals("/"))
+                {
+                    lastOperation = OperationType.Div;
+                    lastArgument = ParseKeyNumber();
+                    newArgument = true;
+                }
+                else if (key.Equals("="))
+                {
+                    int currentArgument = ParseKeyNumber();
+                    if (lastOperation == OperationType.Plus)
+                    {
+                        display = (lastArgument + currentArgument).ToString();
+                    }
+
+                    if (lastOperation == OperationType.Div && currentArgument == 0)
+                    {
+                        display = "Division By Zero Error";
+                    }
+                    shouldReset = true;
+                }
+                else
+                {
+                    if (shouldReset)
+                    {
+                        display = "";
+                        shouldReset = false;
+                    }
+                    if (newArgument)
+                    {
+                        display = "";
+                        newArgument = false;
+                    }
+                    display += key;
+                }
+            }
+        }
+
+        private int ParseKeyNumber()
+        {
+            if (display.Equals(""))
+                return 0;
+            return Int32.Parse(display);
+        }
+
+        public String GetDisplay()
+        {
+            if (display.Equals(""))
+                return "0";
+            return display;
+        }
+    }
+
+}
+
---
+++ C#/E3.AddFunctionality/S1.Refactor/S1.guard/OperationType.cs
@@ -0,0 +1,8 @@
+﻿namespace TDDLegacyCode.E3.AddFunctionality.S1.Refactor.S1.Guard
+{
+    public enum OperationType
+    {
+        Plus,
+        Div
+    }
+}
